f43e7e7521e06b43dc8dc428861c357f579ca20d
Francisco Figueiredo <francisco-abf@hotmail.com>
2026-04-24 00:14:02 +0100

    pricing routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   root "home#index"
+  get "pricing" => "pricing#index"
 
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest

bff237403c77ed1cefc5a5d8baf5bfa4245a1433
Francisco Figueiredo <francisco-abf@hotmail.com>
2026-09-03 01:35:56 +0100

    carousel on our space

--- app/controllers/our_space_controller.rb
+++ app/controllers/our_space_controller.rb
@@ -1,5 +1,6 @@
 class OurSpaceController < ApplicationController
   def index
     @unavailable_desks = SeatAvailability.unavailable_desk_codes(Date.current)
+    @carousel_images = (1..9).map { |index| "fotos/imagem#{index}.jpeg" }
   end
 end
